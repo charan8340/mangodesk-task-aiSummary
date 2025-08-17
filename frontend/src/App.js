e8506656d026e5c6bc0227ed627cf2e885830402
charan <saicharan.maidi@gmail.com>
2025-08-17 16:49:16 +0530

    updated deployment links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,3 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
 import { useState } from "react";
 
 function App() {
@@ -31,8 +6,10 @@ function App() {
   const [summary, setSummary] = useState("");
   const [recipients, setRecipients] = useState("");
 
+
+
   const generateSummary = async () => {
-    const res = await fetch("http://localhost:5000/generate-summary", {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+    const res = await fetch("https://mangodesk-task-aisummary-4.onrender.com/generate-summary", {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ transcript, instruction }),
@@ -42,7 +19,7 @@ function App() {
   };
 
   const sendEmail = async () => {
-    const res = await fetch("http://localhost:5000/send-email", {
+    const res = await fetch("https://mangodesk-task-aisummary-4.onrender.com/send-email", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ summary, recipients: recipients.split(",") }),
